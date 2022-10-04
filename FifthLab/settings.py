992825ea24bbc658db8ffb881fc399cf06208935
vagabund3219 <vagabund3219@gmail.com>
2022-10-04 14:53:40 +0300

    sixth lab

--- FifthLab/settings.py
+++ FifthLab/settings.py
@@ -113,11 +113,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
+import os
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
